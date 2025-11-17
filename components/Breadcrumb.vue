fb83fceba87bc439414d29b285e4e67593c98465
jonathan-martz <github@jmartz.de>
2025-11-17 20:44:43 +0100

    change label to code

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -7,7 +7,7 @@
       </li>
       <li v-for="breadcrumb in breadcrumbs" :key="breadcrumb.id">
         <Fa :icon="['fas', breadcrumb.icon ?? 'question']" class="mr-3"/>
-        <a :href="'/de/' + breadcrumb.link" class="">{{ $t('breadcrumb.'+breadcrumb.slug)}}</a>
+        <a :href="'/de/' + breadcrumb.link" class="">{{ $t('breadcrumb.'+breadcrumb.code)}}</a>
       </li>
     </ul>
   </div>
